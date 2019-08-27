f040841a12f6d5de8de9f6aed3c4e3f595774203
BuildTools <unconfigured@null.spigotmc.org>
2019-08-27 10:11:15 +0200

    End of section 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,21 +11,20 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any  = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private aleritfy: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register()  {
     this.authService.register(this.model).subscribe(() => {
-      console.log('Regstration succesful'); }
+      this.aleritfy.success('Regstration succesful'); }
       , error => {
-        console.log(error);
+        this.aleritfy.error(error);
       });
   }
 
   cancel()  {
     this.cancelRegister.emit(false);
-    console.log('Cancelled');
   }
 }
